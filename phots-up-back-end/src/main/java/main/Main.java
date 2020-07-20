28b77af6d6369fe92fcc597110df15ec67820ab8
hi-all-homies <kissfm.tratata@gmail.com>
2020-07-20 23:02:13 +0300

    fixes

--- phots-up-back-end/src/main/java/main/Main.java
+++ phots-up-back-end/src/main/java/main/Main.java
@@ -1,10 +1,19 @@
 package main;
 
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import main.model.entities.Post;
 import main.model.entities.User;
 import main.services.user.UserService;
 
@@ -18,10 +27,33 @@ public class Main {
 	
 	
 	@EventListener
-	public void setUser(ApplicationReadyEvent event) {
-		var user = new User(null, "anime-love" , "hello");
-		
+	public void setUser(ApplicationReadyEvent event) throws IOException {
+		var user = new User(null, "i-dont-know" , "password");
 		this.userService.registerUser(user).subscribe();
+		user.setId(1l);
+		var post = new Post();
+		post.setAuthor(user);
+		post.setContent("hello world!!!");
+		
+		var bodyBuilder = new MultipartBodyBuilder();
+		
+		var res = new ClassPathResource("first-image.jpg");
+		var image = res.getInputStream().readAllBytes();
+		var jsonPost = new ObjectMapper().writeValueAsString(post);
+		
+		bodyBuilder.part("post", jsonPost, MediaType.TEXT_PLAIN)
+			.header("Content-Disposition", "form-data; name=post")
+			.header("Content-type", "text/plain");
+		bodyBuilder.part("image", new ByteArrayResource(image))
+			.header("Content-Disposition","form-data; name=image; filename=first-image.jpg");
+		var body = bodyBuilder.build();
+		
+		WebClient client = WebClient.create("http://localhost:8080");
+
+		client.post().uri("/phots/up/api/posts")
+			.contentType(MediaType.MULTIPART_FORM_DATA)
+			.body(BodyInserters.fromMultipartData(body))
+			.exchange()
+			.then().subscribe();
 	}
-	 
 }
